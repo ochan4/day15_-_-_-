81b0a6342db973d4e2a6ae4301b8033384e7523d
tarena <tarena@tarenadeimac-15.local>
2016-04-22 14:03:09 +0800

    Initial Commit

---
+++ 图片编辑项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片编辑项目
+//
+//  Created by tarena on 16/4/22.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片编辑项目/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片编辑项目
+//
+//  Created by tarena on 16/4/22.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
